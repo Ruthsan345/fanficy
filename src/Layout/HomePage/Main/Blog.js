43867b889b13f1e7d70b9f4b01094a10521cdf70
Ruthsan R <ruthruthsan@gmail.com>
2020-10-28 01:58:24 +0530

    Update Blog.js

--- src/Layout/HomePage/Main/Blog.js
+++ src/Layout/HomePage/Main/Blog.js
@@ -36,7 +36,7 @@ const sections = [
 ];
 
 const mainFeaturedPost = {
-  title: 'Title of a longer featured blog post',
+  title: 'FanFicy, the world of Fictiont',
   description:
     "Multiple lines of text that form the lede, informing new readers quickly and efficiently about what's most interesting in this post's contents.",
   image: 'https://source.unsplash.com/random',
