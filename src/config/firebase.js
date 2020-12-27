c22fe246ed8d32643f7645374d38c00d56e9192b
Ruthsan R <ruthruthsan@gmail.com>
2020-12-27 20:43:11 +0530

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,6 +1,9 @@
 import firebase from 'firebase/app';
 import 'firebase/storage';
-
+import 'firebase/auth';        // for authentication
+import 'firebase/storage';     // for storage
+import 'firebase/database';    // for realtime database
+import 'firebase/firestore';   // for cloud firestore
 
 
 
